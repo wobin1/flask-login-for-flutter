17abc4af761cab23ebb4c990575f0932bc8e87d0
Nathaniel Dauda Wobin <42215652+wobin1@users.noreply.github.com>
2021-11-03 22:50:45 +0100

    added remote database

--- software/__init__.py
+++ software/__init__.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'something only you know'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/reglog_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cnmaanlbnytyje:ed87fcfe28d3ac14fd4fc8ffce12bc1540e036af08581184450ddd1f0a2478ef@ec2-18-210-233-138.compute-1.amazonaws.com:5432/d6d2sieslt4udv'
     app.config['SQLALCHEM_TRACK_MODIFICATION'] = False
 
     db.init_app(app)
